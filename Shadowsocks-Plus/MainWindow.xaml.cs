78c024c3bdda176c17d1578536d066aca346bad9
jm33-m0 <jm33.m0@gmail.com>
2016-08-28 13:43:30 +0800

    removed unused usings

--- Shadowsocks-Plus/MainWindow.xaml.cs
+++ Shadowsocks-Plus/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using System.Threading;
 using System.Diagnostics;
 using System.Net;
 using Microsoft.Win32;
-using System.ComponentModel;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Shadowsocks_Plus
 {
